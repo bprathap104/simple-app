71c16b16547b1172ba15db663e3760d9513184cb
Saravanan <57014443+bprathap104@users.noreply.github.com>
2024-04-22 04:50:05 +0530

    Codedeploy setup

--- scripts/start_frontend.sh
+++ scripts/start_frontend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pm2 start /home/frontend/src/server.js
+/home/frontend/bin/node_modules/pm2/bin/pm2 start /home/frontend/src/server.js
--- scripts/stop_frontend.sh
+++ scripts/stop_frontend.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pm2 stop /home/frontend/src/server.js
+/home/frontend/bin/node_modules/pm2/bin/pm2 stop /home/frontend/src/server.js
